ee3a094d62626662698e159a855525c4ce19b400
Valeria Pagliarini <valepagliar@gmail.com>
2025-03-27 15:39:57 +0000

    js table

--- assets/js/d.js
+++ assets/js/d.js
@@ -12,5 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
     let randomNum = Math.floor(Math.random() * 99) + 1;
     const cellIndex = table.children[randomNum];
     cellIndex.classList.add("background");
+
+    const p = document.getElementById("numeri estratti");
+    if (p.textContent === "Numeri estratti:") {
+      p.textContent += randomNum + 1;
+    } else {
+      p.textContent += "," + (randomNum + 1);
+    }
   });
+  const secondTable = document.getElementById("secondtable");
+  for (i = 1; i <= 24; i++) {
+    const secondTableCell = document.createElement("div");
+    secondTableCell.textContent = i;
+    secondTableCell.className = "secondCell";
+    secondTable.appendChild(secondTableCell);
+  }
 });
